c5f9863e29106fe48aabd81c5e4bfaf40347672a
Ducanh <phamducanh08022006@gmail.com>
2025-12-13 16:20:50 +0700

    Added getter setter, not yet finished

--- 3.student.mark.oop.math.py
+++ 3.student.mark.oop.math.py
@@ -5,43 +5,83 @@ from curses import wrapper
 
 class thing:
     def __init__(self, name, id):
-        self.name = name
-        self.id = id
+        self._name = name
+        self._id = id
         
     def display(self):
-        print(f"Name: {self.name}")
-        print(f"ID: {self.id}")
+        print(f"Name: {self._name}")
+        print(f"ID: {self._id}")
+    
+    #Getters   
+    def get_name(self):
+        return self._name
+    
+    def get_ID(self):
+        return self._id
+    
+    #Setters
+    def set_name(self, name: str):
+        self._name = name
+        
+    def set_id(self, id: str):
+        self._id = id
         
 class student(thing):
     def __init__(self, name, id, DoB):
         super().__init__(name, id)
-        self.DoB = DoB
-        self.mark = {}
-        self.GPA = 0
+        self.__DoB = DoB
+        self.__mark = {}
+        self.__GPA = 0
         
     def display_info(self):
-        print(f"Name: {self.name}")
-        print(f"ID: {self.id}")
-        print(f"Date of Birth: {self.DoB}")
+        print(f"Name: {self._name}")
+        print(f"ID: {self._id}")
+        print(f"Date of Birth: {self.__DoB}")
         print()
 
     def display_score(self):
-        for k,v in self.mark.items():
+        for k,v in self.__mark.items():
             print(f"Subject ID: {k}")
             print(f"Mark: {v}")
             print()
             
-        print(f"GPA: {self.GPA:.1f}")
+        print(f"GPA: {self.__GPA:.1f}")
+        
+    #Getters
+    def get_DoB(self):
+        return self.__DoB
+    
+    def get_GPA(self):
+        return self.__GPA
+    
+    def get_mark(self):
+        return self.__mark
+    
+    #Setters
+    def set_DoB(self, DoB: str):
+        self.__DoB = DoB
+        
+    def set_GPA(self, GPA: float):
+        self.__GPA = GPA
+        
+    def set_mark(self, id: str, mark: float):
+        self.__mark.update({id: mark})
         
 class course(thing):
     def __init__(self, name, id, cred):
         super().__init__(name, id)
-        self.credit = cred
+        self.__credit = cred
         
     def display(self):
         super().display()
-        print(f"Credits: {self.credit}")
+        print(f"Credits: {self.__credit}")
         print()
+        
+    def get_credit(self):
+        return self.__credit    
+    
+    def set_credit(self, cred):
+        self.__credit = cred    
         
 class list:
     def __init__(self):
@@ -85,13 +125,13 @@ class mark_manage:
 
     def __find_student(self, sid):
         for i in self.student_list:
-            if i.id == sid:
+            if i.get_ID() == sid:
                 return i
         return None
     
     def __find_course(self, cid):
         for i in self.course_list:
-            if i.id == cid:
+            if i.get_ID() == cid:
                 return i
         return None
 
@@ -102,8 +142,9 @@ class mark_manage:
         
         if not self.__find_student(sid) is None:
             s = self.__find_student(sid)
-            s.mark.update({cid: math.floor(mark*10)/10})
-            s.GPA = self.__update_GPA(sid)
+            #s.mark.update({cid: math.floor(mark*10)/10})
+            s.set_mark(cid, math.floor(mark*10)/10)
+            s.GPA = s.set_GPA(self.__update_GPA(sid))
             
         else:
             raise ValueError("Student ID not found")
@@ -115,10 +156,10 @@ class mark_manage:
         
         cal = []
         
-        for c,m in s.mark.items():
+        for c,m in s.get_mark().items():
             for i in self.course_list:
-                if c == i.id:
-                    cal += [m] * i.credit
+                if c == i.get_ID():
+                    cal += [m] * i.get_credit()
                     
         store = np.array(cal)
         return np.average(store)
